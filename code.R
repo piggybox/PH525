ee45a4e24ea193e132e6e7387bb63bdbad99c184
Sky Yin <sky.yin@gmail.com>
2015-09-11 02:09:35 -0700

    End of week 2

--- code.R
+++ code.R
@@ -22,4 +22,25 @@ for (i in 1:n) {
 
 # better version
 null = replicate(10000, mean(sample(population[,1], 12)) - mean(sample(population[,1], 12)))
+hist(null)
+abline(v=diff, col="red")
 
+# Gapminder data set
+library(devtools)
+install_github("jennybc/gapminder")
+library(gapminder)
+data(gapminder)
+
+y1952 <- gapminder[gapminder$year == 1952,]
+mean(y1952$lifeExp <= 40) # trick to calculate empirical cumulative density
+
+# To mimic plot(ecdf(x))
+x <- gapminder$lifeExp
+props = sapply(qs, function(q) mean(x <= q))
+
+F = function(q) pnorm(q, mean=mean(x), sd=sd(x))
+
+# To mimic qqnorm(x)
+n = length(x)
+ps = ((1:n) - 0.5)/n
+plot(qnorm(ps), sort(x))
